44f7ee7ca2d43b2aaf8610c878e6243f2a8fc2f9
ABHIRAM S <105904233+892132abhi@users.noreply.github.com>
2026-01-12 16:08:02 +0530

    Time: 8 ms (83.91%), Space: 61.4 MB (34.71%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    let res = s.split(" ")
+    for(let i =0;i<res.length;i++){
+        res[i]=res[i].split("").reverse().join("")
+    }
+    res = res.join(" ")
+    return res
+};
