0c103da7ce84230db02c13588acc4017b9f59631
ABHIRAM S <105904233+892132abhi@users.noreply.github.com>
2026-01-22 16:12:48 +0530

    Time: 0 ms (100%), Space: 12.5 MB (14.38%) - LeetHub

---
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        res = [a,b]
+        return sum(res)
+        
