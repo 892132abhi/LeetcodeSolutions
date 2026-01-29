4509af48253da5cf4f65858734ccb81d0acd87e8
ABHIRAM S <105904233+892132abhi@users.noreply.github.com>
2026-01-29 18:18:24 +0530

    Time: 0 ms (100%), Space: 12.5 MB (18.21%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sumOfUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        res = 0
+        for i in nums:
+            if nums.count(i)==1:
+                res+=i
+        return res
