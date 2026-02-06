f5c60ca922e401e282bda8f6bb3cb3f56a038f6e
ABHIRAM S <105904233+892132abhi@users.noreply.github.com>
2026-02-06 09:59:11 +0530

    Time: 60 ms (40.65%), Space: 12.8 MB (27.1%) - LeetHub

--- 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.py
+++ 2535-difference-between-element-sum-and-digit-sum-of-an-array/2535-difference-between-element-sum-and-digit-sum-of-an-array.py
@@ -4,13 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        res=[]
-        ans = sum(nums)
-        for i in nums:
-            a = str(i)
-            if len(a)>1:
-                for j in a:
-                    res.append(int(j))
-            else:
-                res.append(int(a))
-        return abs(ans-sum(res))
+        num1 =[str(i) for i in nums]
+        num2 = "".join(num1)
+        num3 = list(num2)
+        num4 = [int(i) for i in num3]
+        sm = sum(nums)
+        sm2 = sum(num4)
+        return abs(sm-sm2)
